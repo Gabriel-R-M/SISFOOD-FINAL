939af3608f587d23e1a4af95a610bfb5e58e8ccf
Diogo <diogomastrangelo@uol.com.br>
2018-12-12 09:12:37 -0200

    fiscal01

---
+++ fiscal/inicializa_sat.php
@@ -0,0 +1,32 @@
+<?php
+	
+
+	$caminho='c:\ACBrMonitorPLUS';
+	@unlink("$caminho\sai.txt");	
+
+	///ATIVA O ECF///
+	$ecf = 'SAT.Inicializar';
+	$fp = fopen("$caminho\ENT.txt", "w");
+	$escreve = fwrite($fp, $ecf);
+	fclose($fp); 			
+
+
+	$x=1;
+	while($x==1){						
+		if(file_exists("$caminho\sai.txt")){
+			$x=2;
+			//LÊ O ARQUIVO DE RESPOSTA//
+			$ponteiro = fopen ("$caminho\sai.txt","r");
+			echo $linha = trim(fgets($ponteiro));		
+			fclose($ponteiro);	
+			unlink("$caminho\sai.txt");	
+			exit();				
+		} else {
+			sleep(.2);			
+		}			
+	}				
+	///ATIVA O ECF///
+		
+
+		
+?>
