f1f381fa374e973f25e4d31e4bc272f1ed68417f
Diogo Mastrangelo <diogomastrangelo@uol.com.br>
2019-04-02 23:01:15 -0300

    ok
    ok

--- javascript/fiscal.js
+++ javascript/fiscal.js
@@ -26,39 +26,6 @@ function venda_fiscal(){
 	muda_mensagem_fiscal("Aguarde, inicializando equipamento...");
 	$.post('fiscal/inicializa_sat.php',{inicializa:1}, function(resposta_fiscal){
 
-		if(resposta_fiscal==1){
-														
-			muda_mensagem_fiscal("Emitindo cupom fiscal...");
-			$.post('fiscal/cria_cupom_fiscal.php',{cpf_cliente:cpf_cliente}, function(resposta_fiscal){
-				
-				if(resposta_fiscal==1){
-
-					muda_mensagem_fiscal("Transmitindo cupom...");
-					$.post('fiscal/envia_cupom_sat.php',{venda_fiscal:venda_fiscal}, function(resposta_fiscal){
-						
-
-													
-						alert(resposta_fiscal)
-																
-
-					});	
-					
-
-				//ERRO AO CRIAR CUPOM//	
-				} else {
-					$("#botao_erro_sat").show();
-					muda_mensagem_fiscal("<h4>Erro:</h4>"+resposta_fiscal);				
-				}
-												
-
-			});	
-
-										
-		//ERRO AO INICIALIZAR O SAT//
-		} else {
-			$("#botao_erro_sat").show();
-			muda_mensagem_fiscal("<h4>Erro:</h4> Problema ao inicializar equipamento.");				
-		}
 
 	});
 }
